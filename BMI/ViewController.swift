b768133c6f4b63888110fd7ee2e8362e0fc18f51
douglas manicka <douglasmanicka@gmail.com>
2020-03-04 23:17:40 -0300

    add bmi calculus

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFieldHeigth: UITextField!
     @IBOutlet weak var labelResult: UILabel!
     @IBOutlet weak var imageViewResult: UIImageView!
+    @IBOutlet weak var uiViewResult: UIView!
+    
+    
+    var bmi: Double = 0
     
     
     override func viewDidLoad() {
@@ -21,8 +25,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculate(_ sender: Any) {
+        guard let textWeight = textFieldWeight.text, !textWeight.isEmpty, let  weight = Double(textWeight.replacingOccurrences(of: ",", with: "."))  else { return}
+        guard let textHeigth = textFieldHeigth.text, !textHeigth.isEmpty, let  height = Double(textHeigth.replacingOccurrences(of: ",", with: "."))  else { return}
+        bmi = weight / pow(height, 2)
+        showResults()
     }
     
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func showResults(){
+        var result: String = ""
+        var image: String =  ""
+        switch bmi {
+            case 0..<16:
+                result = "Magreza"
+                image =  "magreza"
+            case 16..<18.5:
+                result = "Abaixo do Peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobre Peso"
+                image = "sobre"
+          
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        labelResult.text = "\(Int(bmi)) : \(result)"
+        imageViewResult.image = UIImage(named: image)
+        uiViewResult.isHidden = false
+        view.endEditing(true)
+    }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BMI-CALCULATOR
+
+The infamous body mass index calculator(BMI) in Brazilian Portuguese(IMC)!
+
+
+<p align="center"><img src="/screenshots/screen1.png" width="30%" height="30%" />
+<img src="/screenshots/screen2.png" width="30%" height="30%" />
+<img src="/screenshots/screen3.png" width="30%" height="30%" /></p>
+
+<p align="center"><img src="/screenshots/screen4.png" width="30%" height="30%" />
+<img src="/screenshots/screen5.png" width="30%" height="30%" /></p>
